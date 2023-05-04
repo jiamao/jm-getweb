06a87acd19c7c1e21ecdaa55ce2a29583cc3476f
jiamao <haofefe@163.com>
2023-05-04 11:36:46 +0800

    :art: error

--- tasks/baike.js
+++ tasks/baike.js
@@ -88,18 +88,18 @@ async function getPageMeta(page) {
         try {
           window.scrollTo(0, document.body.scrollHeight);
           
-          document.querySelector('.lemmaWgt-searchHeader').remove();
-          document.querySelector('.header-wrapper').remove();
-          document.querySelector('.navbar-wrapper').remove();
-          document.querySelector('.before-content').remove();
-          document.querySelector('.side-content').remove();
-          document.querySelector('.top-tool').remove();
-          document.querySelector('.new-bdsharebuttonbox').remove();
-          document.querySelector('.tashuo-bottom').remove();
-          document.querySelector('.after-content').remove();
-          document.querySelector('.wgt-footer-main').remove();
-          document.querySelector('.main-content').style.width='95%';
-          document.querySelector('.content').style.width='90%';
+          document.querySelector('.lemmaWgt-searchHeader') && document.querySelector('.lemmaWgt-searchHeader').remove();
+          document.querySelector('.header-wrapper') && document.querySelector('.header-wrapper').remove();
+          document.querySelector('.navbar-wrapper') && document.querySelector('.navbar-wrapper').remove();
+          document.querySelector('.before-content') && document.querySelector('.before-content').remove();
+          document.querySelector('.side-content') && document.querySelector('.side-content').remove();
+          document.querySelector('.top-tool') && document.querySelector('.top-tool').remove();
+          document.querySelector('.new-bdsharebuttonbox') && document.querySelector('.new-bdsharebuttonbox').remove();
+          document.querySelector('.tashuo-bottom') && document.querySelector('.tashuo-bottom').remove();
+          document.querySelector('.after-content') && document.querySelector('.after-content').remove();
+          document.querySelector('.wgt-footer-main') && document.querySelector('.wgt-footer-main').remove();
+          document.querySelector('.main-content') && document.querySelector('.main-content').style.width = '95%';
+          document.querySelector('.content') && document.querySelector('.content').style.width = '90%';
           
           obj = {
             url: location.href,
